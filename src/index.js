53e588812484776cb86fe6e6985f133f7a881457
Ivan Risovic <ivan.risovic@vuv.hr>
2022-05-19 12:23:26 +0200

    react

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Pocetna from "./routes/Pocetna"
+import Dodaj from './routes/Dodaj';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigacija from './routes/Navigacija';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render(  
+    <BrowserRouter>
+    <Navigacija />
+      <Routes>
+        <Route path='/' element={<Pocetna />}/>  
+        <Route path='/routes/Dodaj' element={<Dodaj />}/>         
+      </Routes>    
+    </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
